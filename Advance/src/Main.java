7a99d421e5f1c243d33c9a97bef9fc8097b8bb8a
ispasov26 <isploon@gmail.com>
2025-05-27 10:37:24 +0300

    Initialization

---
+++ Advance/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Events{
+
+}
+
+class Enemy extends Events{
+
+}
+
+class Treasure extends Events{
+
+}
+
+class Fountain extends Events{
+
+}
+
+class Pitfall extends Events{
+
+}
+
+class Wizard extends Events{
+
+}
+
+class Items{
+
+}
+
+class Combat{
+
+}
+
+class Player {
+
+}
