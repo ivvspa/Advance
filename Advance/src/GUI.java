d069065e4bf44a2de335c44696abbf3e7171a8a2
ispasov26 <isploon@gmail.com>
2025-06-11 04:17:22 +0300

    Final probably

--- Advance/src/GUI.java
+++ Advance/src/GUI.java
@@ -1,22 +1,210 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.Math;
+import java.util.ArrayList;
 
 public class GUI extends JFrame {
     private JPanel panel1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JTextArea textArea1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
+    private JButton save;
+    private JButton advance;
+    private JButton set;
+    private JTextArea textArea;
+    private JComboBox<Items> selection = new JComboBox<Items>();
+    private JButton use;
+    private JButton delete;
+    private JButton read;
+    private JTable table1;
+    private JButton load;
+    private JLabel stats;
+    private Events randomEvent;
+    private Player p1 = new Player (14, 1);
 
 
-    public GUI() {
+    public GUI() {  //here is mostly set-ups and creating objects which I can use
         setTitle("Advance");
-        setSize(700, 500);
+        setSize(900, 600);
         setContentPane(panel1);
         setVisible(true);
+
+        Enemy gwishin = new Enemy(0.25, 2,5, "Gwishin", "Revenge", "Petrified", 8);
+        Enemy gumiho = new Enemy(0.15, 5, 10, "Gumiho", "Liver Extractor", "Charming Smile", 6);
+        Enemy daltokki = new Enemy(0.15, 15, 18, "Daltokki", "Rice Cake Throw", "Moon Haunting", 2);
+        Enemy yong = new Enemy (0.05, 10, 12, "Yong", "Tail Whip", "Water Hurricane", 3);
+        Enemy saja = new Enemy (0.01, 20, 30,"Jeosung Saja", "Reaper Scythe", "Death Knocks on the Door", 10);
+
+        Enemy[] collective = {gwishin, gumiho, daltokki, yong, saja};
+
+        Items healingPotion = new Items ("Healing Potion", "heal", 10, "A simple potion that restores your health. Stacking up on them seems like a good idea.");
+        Items powerUp = new Items ("Power-up potion", "add", 10, "A big glass of blue, sparkly lemonade. Looks like it might give you some strength.");
+        Items bronzeSpear = new Items ("Bronze Spear", "damage", 5, "Big pointy stick with a fork on top. Not very useful, but sufficient for your survival.");
+        Items silverSpear = new Items ("Silver Spear", "damage", 8, "A well-crafted weapon. Its speed is formidable in comparison to its damage.");
+        Items goldenSpear = new Items ("Golden Spear", "damage", 14, "A weapon crafted by a professional. Its speed is unremarkable, but its damage is really good.");
+        Items legendarySpear = new Items ("Legendary Spear", "damage", 20, "The fabled weapon. Heavy as it is, it deals extraordinary damage.");
+
+        Treasure treasure1 = new Treasure(0.07, healingPotion);
+        Treasure treasure2 = new Treasure(0.03, powerUp);
+        Treasure treasure3 = new Treasure(0.02, bronzeSpear);
+        Treasure treasure4 = new Treasure(0.04, silverSpear);
+        Treasure treasure5 = new Treasure(0.03, goldenSpear);
+        Treasure treasure6 = new Treasure(0.01, legendarySpear);
+
+        Items treasureItem = new Items("", "", 0, ""); //for set button
+
+        Fountain fountain = new Fountain(0.09);
+
+
+
+        Inventory inv = new Inventory (new ArrayList<Items>());
+        inv.getInv().add(bronzeSpear);
+        selection.addItem(bronzeSpear);
+
+        Object[][] data = new Object[6][2];
+
+        ArrayList<Events> events = new ArrayList<Events>();
+        events.add(gwishin);
+        events.add(gumiho);
+        events.add(daltokki);
+        events.add(yong);
+        events.add(saja);
+        events.add(treasure1);
+        events.add(treasure2);
+        events.add(treasure3);
+        events.add(treasure4);
+        events.add(treasure5);
+        events.add(treasure6);
+        events.add(fountain);
+
+        inv.displayItems(inv.getInv(), table1, data);
+
+        stats.setText("Level:" +p1.getLevel());
+
+
+
+        advance.addActionListener(new ActionListener() {  //the advance button, where the progression is kept score off, events are randomized and pulled off
+            public void actionPerformed(ActionEvent e) {
+
+                stats.setText("Level:" +p1.getLevel());
+
+                if (p1.getHealth()>0) {
+                    p1.setLevel(p1.getLevel() + 1);
+
+
+                    p1.setHealth(p1.getHealth());
+
+                    healingPotion.setEffect(healingPotion.getEffect(), p1);
+                    powerUp.setEffect(powerUp.getEffect(), p1);
+                    bronzeSpear.setEffect(bronzeSpear.getEffect(), p1);
+                    silverSpear.setEffect(silverSpear.getEffect(), p1);
+                    goldenSpear.setEffect(goldenSpear.getEffect(), p1);
+                    legendarySpear.setEffect(legendarySpear.getEffect(), p1);
+
+                    for (Enemy enemy : collective) {
+                        enemy.setHealth(enemy.getHealth(), p1);
+                        enemy.setDamage1(enemy.getDamage1(), p1);
+                        enemy.setDamage2(enemy.getDamage2(), p1);
+                    }
+
+                    randomEvent = Main.eventPicker(events);
+                    if (randomEvent instanceof Enemy) {
+                        textArea.setText("A wild " + ((Enemy) randomEvent).getName() + " appears!");
+                        if (((Enemy) randomEvent).getHealth() > 0) {
+                            advance.setEnabled(false);
+                        }
+                    } else if (randomEvent instanceof Fountain) {
+                        textArea.setText("A small stone fountain appears before your very next step. A small spring of refreshing water is all you needed.");
+                        ((Fountain) randomEvent).fountain(p1, textArea);
+                    } else {
+                        textArea.setText("A treasure chest full of gold stands before you. You find a: \n");
+                        textArea.append(((Treasure) randomEvent).getItem().getName() + "\n");
+                        if (inv.size() < 7) {
+
+                            inv.addItem(((Treasure) randomEvent).getItem(), selection, textArea);
+                            selection.addItem(((Treasure) randomEvent).getItem());
+                            inv.displayItems(inv.getInv(), table1, data);
+
+
+                        } else {
+                            treasureItem.setName(((Treasure) randomEvent).getItem().getName());
+                            treasureItem.setGimmick(((Treasure) randomEvent).getItem().getGimmick());
+                            treasureItem.setEffect(((Treasure) randomEvent).getItem().getEffect());
+                            treasureItem.setDescription(((Treasure) randomEvent).getItem().getDescription());
+                        }
+                    }
+                }
+                else {
+                    advance.setEnabled(true);
+                    p1.setLevel(1);
+                }
+
+            }
+        });
+
+        use.addActionListener(new ActionListener() {  //the fighting mechanic
+            public void actionPerformed(ActionEvent e) {
+
+                if (randomEvent instanceof Enemy) {
+                    Items i = (Items) selection.getSelectedItem();
+                    i.action(p1, ((Enemy)randomEvent), selection, textArea);
+                    if (((Enemy) randomEvent).getHealth() > 0) {
+                        ((Enemy) randomEvent).randomAttack(p1, textArea);
+                    }
+                    else{
+                        advance.setEnabled(true);
+                        textArea.append("\n" + ((Enemy)randomEvent).getName() + " has fallen!");
+                    }
+                }
+
+            }
+        });
+
+        delete.addActionListener(new ActionListener() { //delete function
+            public void actionPerformed(ActionEvent e) {
+                Items selected = (Items) selection.getSelectedItem();
+                inv.removeItem(selected, textArea, selection);
+                selection.removeItem(selected);
+                inv.displayItems(inv.getInv(), table1, data);
+
+            }
+        });
+
+        read.addActionListener(new ActionListener() {  //reading function
+            public void actionPerformed(ActionEvent e) {
+
+                inv.readItem(selection, textArea);
+
+            }
+        });
+
+        set.addActionListener(new ActionListener() {  //setting function
+            public void actionPerformed(ActionEvent e) {
+
+                inv.updateItem(treasureItem, inv, selection);
+                inv.displayItems(inv.getInv(), table1, data);
+
+            }
+        });
+
+        save.addActionListener(new ActionListener() {   //saving function
+            public void actionPerformed(ActionEvent e) {
+
+                Main.save("save.txt", p1, inv);
+                textArea.append("\nSaved");
+
+            }
+        });
+
+        load.addActionListener(new ActionListener() {  //loading function
+            public void actionPerformed(ActionEvent e) {
+
+                p1 = Main.load("save.txt");
+                textArea.setText("Loaded save");
+                advance.setEnabled(true);
+
+            }
+        });
+
+
     }
-}
+    }
+
--- Advance/src/Main.java
+++ Advance/src/Main.java
@@ -1,11 +1,66 @@
 import javax.swing.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.table.DefaultTableModel;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main class
 
     new GUI();
 
     }
+
+    public static Events eventPicker (ArrayList<Events> events){ //method which randomly selects an event
+
+
+        double totalProbability = 0;
+        for (Events event : events) {
+            totalProbability += event.getProbability();
+        }
+
+        double randomValue = Math.random() * totalProbability;
+
+        for (Events event : events) {
+            randomValue -= event.getProbability();
+            if (randomValue <= 0) {
+                return event;
+            }
+        }
+        return events.get(events.size() - 1);
+
+    }
+
+    public static Player load(String path) { //printing/saving
+        Player p1 = new Player (0, 0);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line1 = br.readLine();
+
+            while(line1!=null) {
+                int health = Integer.parseInt(line1);
+                int level = Integer.parseInt(br.readLine());
+                p1.setHealth(health);
+                p1.setLevel(level);
+                line1 = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return p1;
+    }
+
+
+    public static void save(String path, Player p1, Inventory inv) { //reading/loading
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            bw.write(p1.getHealth() + "\n");
+            bw.write(p1.getLevel() + "\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
 class Events{
@@ -13,52 +68,312 @@ class Events{
     private double probability;
     public Events(double probability){
         this.probability = probability;
+
     }
+
+    public double getProbability(){
+        return probability;
+    } //the weighted possibilities
+
 }
 
 class Enemy extends Events{
 
-    public Enemy(double probability) {
+    private String name;
+    private String attackName1;
+    private String attackName2;
+    private int damage1;
+    private int damage2;
+    private int health;
+
+
+    public Enemy(double probability, int damage1, int damage2, String name, String attackName1, String attackName2, int health) { //attributes of enemies
         super(probability);
+        this.damage1 = damage1;
+        this.damage2 = damage2;
+        this.name = name;
+        this.attackName1 = attackName1;
+        this.attackName2 = attackName2;
+        this.health = health;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getDamage1(){
+        return damage1;
+    }
+
+    public int getDamage2() {
+        return damage2;
+    }
+
+    public void setHealth(int health, Player p1) {
+        this.health = health + p1.getLevel();
+    }
+
+    public void setDamage1(int damage1, Player p1) {
+        this.damage1 = damage1;
+    }
+
+    public void setDamage2(int damage2, Player p1) {
+        this.damage2 = damage2;
+    }
+
+    public void attack1(Player p, JTextArea textArea) {
+        textArea.append("\n" + name + " used " + attackName1 + "!");
+        p.takeDamage(damage1 + p.getLevel()/2, textArea);
+    }
+
+    public void attack2(Player p, JTextArea textArea) {
+        textArea.append("\n" + name + " used " + attackName2 + "!");
+        p.takeDamage(damage2 + p.getLevel()/2, textArea);
+    }
+
+    public void takeDamage(int damage, JTextArea textArea) {
+        health = health - damage;
+        textArea.append("\n" + name + " has taken " + damage + " damage.");
+        if (health <= 0) {
+            textArea.append("\n" + name + " has died!");
+        }
+    }
+
+    public void randomAttack(Player p, JTextArea textArea) { //chooses out of the two available attacks
+        if (Math.random() < 0.5) {
+            attack1(p, textArea);
+        } else {
+            attack2(p, textArea);
+        }
+    }
+
 }
 
 class Treasure extends Events{
 
-    public Treasure(double probability) {
+    private Items item;
+
+    public Treasure(double probability, Items item) { //where you get items from
         super(probability);
+        this.item = item;
     }
+    
+    public Items getItem(){
+        return item;
+    }
+    
 }
 
 class Fountain extends Events{
 
     public Fountain(double probability) {
         super(probability);
+    } //healing
+
+    public void fountain (Player p, JTextArea textArea) {
+        p.healDamage(p.getHealth(), textArea);
     }
-}
 
-class Pitfall extends Events{
-
-    public Pitfall(double probability) {
-        super(probability);
-    }
-}
-
-class Wizard extends Events{
-
-    public Wizard(double probability) {
-        super(probability);
-    }
 }
 
 class Items{
 
+    private String name;
+    private String gimmick;
+    private int effect;
+    private String description;
+
+
+    public Items (String name, String gimmick, int effect, String description) {  //attributes of items
+        this.name = name;
+        this.gimmick = gimmick;
+        this.effect = effect;
+        this.description = description;
+
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGimmick(){
+        return gimmick;
+    }
+
+    public int getEffect(){
+        return effect;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+
+    public void setEffect(int effect) {
+        this.effect = effect;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGimmick(String gimmick) {
+        this.gimmick = gimmick;
+    }
+
+    public void setEffect(int effect, Player p1) {
+        this.effect = effect+p1.getLevel();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+    public void action (Player p, Enemy e, JComboBox<Items> aaa, JTextArea textArea){ //main idea that does not really end up working
+        Items selected = (Items) aaa.getSelectedItem();
+        if (this.gimmick.equals("heal")){
+
+            p.healDamage(effect, textArea);
+
+        }
+        if(this.gimmick.equals("damage")){
+            e.takeDamage(effect, textArea);
+        }
+        if (this.gimmick.equals("add")){
+            selected.effect+=p.getLevel();
+            textArea.append("\nYour items feel stronger.");
+        }
+    }
+
 }
 
-class Combat{
+class Inventory {  //all of the sorting/managing happens here
+    private ArrayList<Items> inv;
+
+    public Inventory(ArrayList<Items> inv) {
+        this.inv = inv;
+    }
+
+    public ArrayList<Items> getInv() {
+        return inv;
+    }
+
+    public int size() {
+        return inv.size();
+    }
+
+    public void addItem(Items item, JComboBox<Items> aaa,JTextArea textArea) {
+        if (inv.size() <= 6){
+            inv.add(item);
+            aaa.addItem(item);
+
+        }
+        else {
+            textArea.append("\nInventory is full! Try replacing the new found equipment with something old.");
+        }
+    }
+
+    public void removeItem(Items item, JTextArea textArea, JComboBox<Items> inventory) {
+        inv.remove(item);
+        textArea.append("\nItem removed!");
+    }
+
+    public void updateItem(Items item, Inventory iv, JComboBox<Items> inventory) {
+        Items selected = (Items) inventory.getSelectedItem();
+        for (int i = 0; i < iv.getInv().size(); i++) {
+            if (iv.getInv().get(i) == selected)
+                inv.set(i, item);
+            DefaultComboBoxModel<Items> model = (DefaultComboBoxModel<Items>) inventory.getModel();
+            model.removeElementAt(i);
+            model.insertElementAt(item, i);
+        }
+
+    }
+
+    public void readItem(JComboBox<Items> inventory, JTextArea textArea) {
+        //search method for the item that is in the box thingy
+        Items selected = (Items) inventory.getSelectedItem();
+        if (selected != null) {
+            textArea.append("\n" + selected.getDescription());
+        } else {
+            textArea.append("\nYou don't have that many items... You want me to read nothing?");
+        }
+        }
+    void displayItems (ArrayList<Items> inventory, JTable table, Object[][] data) {  //displays all items in a table
+
+        String[] columnNames = {"#Number", "Item Name"};
+
+        for (int i = 0; i < inventory.size(); i++) {
+            if (inventory.get(i) != null) {
+                data[i][0] = i + 1;
+                data[i][1] = inventory.get(i).getName();
+            }
+        }
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        table.setModel(model);
+
+    }
+    }
+
+
+class Player {  //the user/the player with their attributes
+
+    private int health;
+    private int level;
+
+    public Player(int health, int level) {
+        this.health = health;
+        this.level = level;
+
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+
+    public void takeDamage(int damage, JTextArea textArea) {
+        health = health - damage;
+        textArea.append("\nYou have taken " + damage + " damage. \nRemaining: " + health);
+        if (health <= 0) {
+            textArea.append("\nThis is the end.");
+            textArea.append("\nFinal level: " + level);
+
+        }
+    }
+    public void healDamage(int heal, JTextArea textArea) {
+        this.health += heal;
+        textArea.append("\nYou healed " + heal + " health!");
+
+        if (health >= level+15) {
+            textArea.append("\nYou are at maximum health.");
+        }
+    }
 
 }
 
-class Player {
 
-}
+
